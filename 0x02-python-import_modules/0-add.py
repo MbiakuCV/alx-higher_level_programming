6a8581efa411edef48147706ce49ef354646e26f
MbiakuCV <mbiakuchiagozie@gmail.com>
2023-05-11 17:08:44 +0000

    Import modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
-from add_0 import add
 
-a = 1
-b = 2
+if __name__ == "__main__":
 
-print("{0} + {1} = {2}".format(a, b, add(a, b)))
+    from add_0 import add
+
+
+    a = 1
+
+    b = 2
+
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
